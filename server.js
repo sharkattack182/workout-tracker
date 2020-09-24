65009b1f82fae6a9ad625d8d0f21fb8a99fdcf36
Matthew Courtney <matthewdc182@gmail.com>
2020-09-23 23:49:18 -0400

    deploying

--- server.js
+++ server.js
@@ -13,12 +13,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+});
 
 require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
-  });
-  
+});
